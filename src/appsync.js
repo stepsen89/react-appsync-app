34d9411bc5bda963bd1263f76b348c136ac490ab
stepsen89 <stephanie.senoner@gmail.com>
2020-01-28 13:48:20 +0000

    configured AppSync API and schema, configuration file added

---
+++ src/appsync.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+  aws_appsync_graphqlEndpoint:
+    "https://zhorgjobofax3ijftoeg2eqooe.appsync-api.eu-west-2.amazonaws.com/graphql",
+  aws_appsync_region: "eu-west-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-yalvkpsdgbbbrpsaiqzbyucx7a"
+};
+
+export default awsmobile;
